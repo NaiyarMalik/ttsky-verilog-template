72d5f6ba117f6d44569aca838a2c79e32babb229
NaiyarMalik <naiyar2006@gmail.com>
2026-07-20 19:17:34 -0400

    Serializer

--- src/Serializer.v
+++ src/Serializer.v
@@ -1,18 +1,20 @@
-module parity_calc # ( parameter WIDTH = 8 )
+
+module Serializer # ( parameter WIDTH = 8 )
 
 (
  input   wire                  CLK,
  input   wire                  RST,
- input   wire                  parity_enable,
- input   wire                  parity_type,
- input   wire                  Busy, 
  input   wire   [WIDTH-1:0]    DATA,
- input   wire                  Data_Valid,
- output  reg                   parity 
+ input   wire                  Enable, 
+ input   wire                  Busy,
+ input   wire                  Data_Valid, 
+ output  wire                  ser_out,
+ output  wire                  ser_done
 );
 
 reg  [WIDTH-1:0]    DATA_V ;
-
+reg  [2:0]          ser_count ;
+              
 //isolate input 
 always @ (posedge CLK or negedge RST)
  begin
@@ -23,32 +25,37 @@ always @ (posedge CLK or negedge RST)
   else if(Data_Valid && !Busy)
    begin
     DATA_V <= DATA ;
-   end 
+   end	
+  else if(Enable)
+   begin
+    DATA_V <= DATA_V >> 1 ;         // shift register
+   end
  end
  
 
+//counter
 always @ (posedge CLK or negedge RST)
  begin
   if(!RST)
    begin
-    parity <= 'b0 ;
+    ser_count <= 'b0 ;
    end
   else
    begin
-    if (parity_enable)
+    if (Enable)
 	 begin
-	  case(parity_type)
-	  1'b0 : begin                 
-	          parity <= ^DATA_V  ;     // Even Parity
-	         end
-	  1'b1 : begin
-	          parity <= ~^DATA_V ;     // Odd Parity
-	         end		
-	  endcase       	 
+      ser_count <= ser_count + 'b1 ;		 
 	 end
+	else 
+	 begin
+      ser_count <= 'b0 ;		 
+	 end	
    end
  end 
 
+assign ser_done = (ser_count == 'b111) ? 1'b1 : 1'b0 ;
+
+assign ser_out = DATA_V[0] ;
 
 endmodule
  
